a14f3a83cb98e37af95f84c74c16949f21384a25
DPanteleev <TheDimok@rambler.ru>
2018-04-04 19:54:54 +0300

    Начало

---
+++ src/TelegaBot.java
@@ -0,0 +1,75 @@
+/**
+ * Created by DPanteleev on 26.03.2018.
+ */
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+
+public class TelegaBot extends TelegramLongPollingBot {
+    GetKeyboard getKeyboard = new GetKeyboard();
+    Text text1 = new Text();
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        try {
+            botsApi.registerBot(new TelegaBot());
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "DimooonBot"; // bot name
+    }
+
+    @Override
+    public void onUpdateReceived(Update e) {
+        Message msg = e.getMessage(); // Это нам понадобится
+        String txt = msg.getText();
+        switch (txt) {
+            case "/Start":
+                sendMsg(msg, "Hello! \\uD83D\\uDC4D");
+                break;
+            case "Назад":
+                sendMsg(msg, "down");
+                break;
+            case "Домой":
+                sendMsg(msg, text1.getTest1());
+                break;
+        }
+    }
+
+    @Override
+    public String getBotToken() {
+        return "516033907:AAGl4zfvz6M_-vXhIF0zFW_-RO8r1y5gp5g";
+    }
+
+    private void sendMsg(Message msg, String text) {
+        SendMessage s = new SendMessage()
+                .setChatId(msg.getChatId())
+                .setText(text);
+
+        GetKeyboard getKeyboard = new GetKeyboard();
+
+        s.setReplyMarkup(new ReplyKeyboardMarkup().setResizeKeyboard(true).setKeyboard(getKeyboard.getKeyboardRowList()));
+
+        try {
+            execute(s);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/Text.java
@@ -0,0 +1,42 @@
+public class Text {
+    private String test1 = "Тестовая надпись1";
+    private String test2 = "Тестовая надпись2";
+    private String test3 = "Тестовая надпись3";
+    private String test4 = "Тестовая надпись4";
+    private String test5 = "Тестовая надпись5";
+    private String test6 = "Тестовая надпись6";
+    private String test7 = "Тестовая надпись7";
+    private String test8 = "Тестовая надпись8";
+
+    public String getTest1() {
+        return test1;
+    }
+
+    public String getTest2() {
+        return test2;
+    }
+
+    public String getTest3() {
+        return test3;
+    }
+
+    public String getTest4() {
+        return test4;
+    }
+
+    public String getTest5() {
+        return test5;
+    }
+
+    public String getTest6() {
+        return test6;
+    }
+
+    public String getTest7() {
+        return test7;
+    }
+
+    public String getTest8() {
+        return test8;
+    }
+}
